038bd275fb5de59e4b1785105d9c39dd4522c32a
cesine <cesine@yahoo.com>
2013-06-10 17:18:50 -0400

    using node.io which was installed locally in the project by npm

--- download.sh
+++ download.sh
@@ -11,12 +11,12 @@ for (( i=$1; i <= $2; i++ ))
 do
   echo "<div class='chapter'>" >> $6
 
-  echo node.io -s save "https://www.youversion.com/en-GB/bible/"$4"/"$3"."$i"."$5 | \
+  echo ./node_modules/node.io/bin/node.io -s save "https://www.youversion.com/en-GB/bible/"$4"/"$3"."$i"."$5 | \
     grep div\ class\=\"label \
     | sed -e 's/L<\/span><span class="sc"><span class="content">ord<\/span>/Lord/g' \
     | sed -e 's/\([^0-9]\)<\/span><span class="\(content\)">/\1     /g' >>  $6
   
-  node.io -s save "https://www.youversion.com/en-GB/bible/"$4"/"$3"."$i"."$5 | \
+  ./node_modules/node.io/bin/node.io -s save "https://www.youversion.com/en-GB/bible/"$4"/"$3"."$i"."$5 | \
     grep div\ class\=\"label \
     | sed -e 's/L<\/span><span class="sc"><span class="content">ord<\/span>/Lord/g' \
     | sed -e 's/\([^0-9]\)<\/span><span class="\(content\)">/\1     /g' >>  $6
